1940e2a23ed90ce1401a1ec15411bc374422e4a1
nafisahnubah <nafisahnubah@gmail.com>
2024-09-24 03:19:45 -0300

    added modal, input validation, and finished home page completely

--- script.js
+++ script.js
@@ -17,29 +17,71 @@ document.getElementById('history-btn').addEventListener('click', function(){
 document.getElementById('donate-btn-1').addEventListener('click', function(){
     const balance = getNumericTextValue('balance');
     const input = getNumericInputValue('input-field-1');
+
+    if(isNaN(input)){
+        alert('Please input valid amount!');
+        return;
+    }
+
     const donatedMoney = getNumericTextValue('donation1');
     const newTotal = input + donatedMoney;
+    const newBalance = balance - input;
+
+    if(newBalance<0){
+        alert('Insufficient balance, cannot donate this amount.');
+        return;
+    }
+    my_modal_5.showModal();
+
     document.getElementById('donation1').innerText = newTotal;
-    document.getElementById('balance').innerText = balance - input;
-    addHistory(donatedMoney, 'Flood at Noakhali, Bangladesh');
+    document.getElementById('balance').innerText = newBalance;
+    addHistory(input, 'Flood at Noakhali, Bangladesh');
 })
 
 document.getElementById('donate-btn-2').addEventListener('click', function(){
     const balance = getNumericTextValue('balance');
     const input = getNumericInputValue('input-field-2');
+
+    if(isNaN(input)){
+        alert('Please input valid amount!');
+        return;
+    }
+
     const donatedMoney = getNumericTextValue('donation2');
     const newTotal = input + donatedMoney;
+    const newBalance = balance - input;
+
+    if(newBalance<0){
+        alert('Insufficient balance, cannot donate this amount.');
+        return;
+    }
+    my_modal_6.showModal()
+
     document.getElementById('donation2').innerText = newTotal;
     document.getElementById('balance').innerText = balance - input;
-    addHistory(donatedMoney, 'Flood Relief in Feni, Bangladesh');
+    addHistory(input, 'Flood Relief in Feni, Bangladesh');
 })
 
 document.getElementById('donate-btn-3').addEventListener('click', function(){
     const balance = getNumericTextValue('balance');
     const input = getNumericInputValue('input-field-3');
+
+    if(isNaN(input)){
+        alert('Please input valid amount!');
+        return;
+    }
+
     const donatedMoney = getNumericTextValue('donation3');
     const newTotal = input + donatedMoney;
+    const newBalance = balance - input;
+
+    if(newBalance<0){
+        alert('Insufficient balance, cannot donate this amount.');
+        return;
+    }
+    my_modal_7.showModal()
+
     document.getElementById('donation3').innerText = newTotal;
     document.getElementById('balance').innerText = balance - input;
-    addHistory(donatedMoney, 'Aid for Injured in the Quota Movement');
+    addHistory(input, 'Aid for Injured in the Quota Movement');
 })
